9ea6efeacbc77d3d0383aa04bf10a5f79971b0e4
Nadya Jackson <nadya.jackson@bryanuniversity.edu>
2022-06-17 15:32:24 -0500

    google font

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ export default function Document(){
     return(
         <Html>
             <Head>
-            <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet"/>
+            <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i&display=optional" rel="stylesheet"/>
             </Head>
             <Main></Main>
             <NextScript></NextScript>
